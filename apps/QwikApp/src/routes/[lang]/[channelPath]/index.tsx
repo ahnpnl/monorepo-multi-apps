08170ce70bd487c55ed51b1ef04c3b0aa4c85547
Anh Pham <anh.pham@cm.com>
2023-08-28 17:45:10 +0200

    Remove unused route

--- apps/QwikApp/src/routes/[lang]/[channelPath]/index.tsx
+++
@@ -1,7 +0,0 @@
-import { component$ } from '@builder.io/qwik';
-
-const LandingPage = component$(() => {
-    return <h1>Landing page</h1>;
-});
-
-export default LandingPage;
--- apps/QwikApp/src/routes/[lang]/index.tsx
+++
@@ -1,5 +0,0 @@
-import type { RequestHandler } from '@builder.io/qwik-city';
-
-export const onRequest: RequestHandler = ({ redirect, params }) => {
-    throw redirect(302, `/en/${params.lang}/`);
-};
