58e45e0326a3842491b10fd27e47a252a437730c
Anh Pham <anh.pham@cm.com>
2024-03-08 20:57:31 +0100

    Switch to pnpm

--- libs/js-extension/src/Array/arrayHelpers.ts
+++
@@ -1,3 +0,0 @@
-export function unique<T>(...items: Array<T>): Array<T> {
-    return Array.from(new Set(items));
-}
--- libs/js-extension/src/I18n/types.ts
+++ libs/js-extension/src/I18n/types.ts
@@ -1,5 +1,5 @@
-import type { PLATFORM_SUPPORTED_LANGUAGES } from 'js-extension/src/I18n/language';
-import type { PLATFORM_SUPPORTED_LOCALES } from 'js-extension/src/I18n/locale';
+import type { PLATFORM_SUPPORTED_LANGUAGES } from './language';
+import type { PLATFORM_SUPPORTED_LOCALES } from './locale';
 
 export type LanguageType = (typeof PLATFORM_SUPPORTED_LANGUAGES)[keyof typeof PLATFORM_SUPPORTED_LANGUAGES];
 export type IETFLocaleType = (typeof PLATFORM_SUPPORTED_LOCALES)[keyof typeof PLATFORM_SUPPORTED_LOCALES];
