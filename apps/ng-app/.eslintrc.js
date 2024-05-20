1c0f58f9435b8fc6c3e5374d77612dd13ce2f6e4
ahnpnl <27772165+ahnpnl@users.noreply.github.com>
2024-05-20 12:54:41 +0700

    infra: configure base eslint config for projects to extend from

---
+++ apps/ng-app/.eslintrc.js
@@ -0,0 +1,50 @@
+const baseConfig = require('../../eslintrc.base');
+
+const overridesConfig = baseConfig.overrides.map((cfg, idx) => {
+    if (idx === 0) {
+        return {
+            ...cfg,
+            extends: [
+                'plugin:@angular-eslint/recommended',
+                'plugin:@angular-eslint/template/process-inline-templates',
+                ...cfg.extends,
+            ],
+            rules: {
+                ...cfg.rules,
+                '@angular-eslint/directive-selector': [
+                    'error',
+                    {
+                        type: 'attribute',
+                        prefix: 'app',
+                        style: 'camelCase',
+                    },
+                ],
+                '@angular-eslint/component-selector': [
+                    'error',
+                    {
+                        type: 'element',
+                        prefix: 'app',
+                        style: 'kebab-case',
+                    },
+                ],
+            },
+        };
+    }
+
+    return cfg;
+});
+
+module.exports = {
+    ...baseConfig,
+    overrides: [
+        ...overridesConfig,
+        {
+            files: ['*.html'],
+            extends: [
+                'plugin:@angular-eslint/template/recommended',
+                'plugin:@angular-eslint/template/accessibility',
+                'plugin:prettier/recommended',
+            ],
+        },
+    ],
+};
